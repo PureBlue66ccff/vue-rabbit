8af27a7b69bb13960dd60f92bd3bad776ef8610a
PureBlue66ccff <2550185445@qq.com>
2023-05-18 15:19:24 +0800

    完成路由整体设计

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
 </script>
 
 <template>
-  <el-row class="mb-4">
-    <el-button plain>Plain</el-button>
-    <el-button type="primary" plain>Primary</el-button>
-    <el-button type="success" plain>Success</el-button>
-    <el-button type="info" plain>Info</el-button>
-    <el-button type="warning" plain>Warning</el-button>
-    <el-button type="danger" plain>Danger</el-button>
-  </el-row>
+  <RouterView></RouterView>
 </template>
 
 <style scoped>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>首页</div>
+  <RouterView></RouterView>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
