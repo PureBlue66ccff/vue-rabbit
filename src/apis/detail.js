6eefcfddd6670b88dc7ac4fa6261cab5e6bc7676
PureBlue66ccff <2550185445@qq.com>
2023-05-22 21:17:53 +0800

    初步完成商品详情

---
+++ src/apis/detail.js
@@ -0,0 +1,22 @@
+import http from '@/utils/http'
+
+
+export const getDetailAPI = (id) => {
+  return http({
+    url: '/goods',
+    params: {
+      id
+    }
+  })
+}
+
+export const getHotGoodsAPI = ({ id, type, limit = 3 }) => {
+  return http({
+    url: '/goods/hot',
+    params: {
+      id,
+      type,
+      limit
+    }
+  })
+}
