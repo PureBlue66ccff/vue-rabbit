39e3ceb900f3f9e6a9716ceaa867a62cb94155ea
PureBlue66ccff <2550185445@qq.com>
2023-06-05 17:45:09 +0800

    完成支付结果页

--- src/views/Pay/index.vue
+++ src/views/Pay/index.vue
@@ -1,7 +1,40 @@
 <script setup>
-import { ref } from "vue";
+import { getOrderAPI } from "@/apis/pay";
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import { useCountDown } from "@/composables/useCountDown";
+
+const { formatTime, start } = useCountDown()
+
+const route = useRoute()
 
 const payInfo = ref({})
+
+const getPayInfo = async () => {
+  const res = await getOrderAPI(route.query.id)
+  payInfo.value = res.result
+  //console.log(res.result);
+
+  //真实数据
+  //start(res.result.countdown)
+  //测试数据
+  start(100)
+}
+
+onMounted(() => getPayInfo())
+
+const timeFlag = ref(false)
+watch(formatTime, (newVal, oldVal) => {
+  if (newVal === '00分00秒') {
+    timeFlag.value = true
+  }
+})
+
+//支付地址
+const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net/'
+const backURL = 'http://127.0.0.1:5173/paycallback'
+const redirectUrl = encodeURIComponent(backURL)
+const payUrl = `${baseURL}pay/aliPay?orderId=${route.query.id}&redirect=${redirectUrl}`
 
 </script>
 
@@ -11,10 +44,11 @@ const payInfo = ref({})
       <!-- 付款信息 -->
       <div class="pay-info">
         <span class="icon iconfont icon-queren2"></span>
-        <div class="tip">
+        <div class="tip" v-if="!timeFlag">
           <p>订单提交成功！请尽快完成支付。</p>
-          <p>支付还剩 <span>24分30秒</span>, 超时后将取消订单</p>
+          <p>支付还剩 <span>{{ formatTime }}</span>, 超时后将取消订单</p>
         </div>
+        <el-alert v-else>超时！将取消订单！</el-alert>
         <div class="amount">
           <span>应付总额：</span>
           <span>¥{{ payInfo.payMoney?.toFixed(2) }}</span>
